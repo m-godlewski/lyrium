42164ddd6453f7d65fa85523e50316588fe5eae6
mateusz.godlewski <mgodlewski25@gmail.com>
2021-09-15 16:06:53 +0200

    config.py file updated

--- backend/config.py
+++ backend/config.py
@@ -6,10 +6,13 @@ Main configuration script.
 import os
 
 
-# ROOT APPLICATION DIRECTORY PATH
+# root application directory path
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-# LOGGING CONFIGURAION
+# path to data storage directory
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
+# logging configuraion
 LOGGING = {
     "CONFIG_FILE": os.path.join(BASE_DIR, ".logs", "logging.conf")
 }
