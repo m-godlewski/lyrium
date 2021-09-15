1472f022d4fdb9756f679aa552cd38990ffa03d0
mateusz.godlewski <mgodlewski25@gmail.com>
2021-09-15 21:22:11 +0200

    analyse endpoint created, artist analyse method added to controller scirpt

---
+++ backend/app/controller.py
@@ -0,0 +1,9 @@
+"""
+Script that contains methods responsible for business logic of application.
+"""
+
+from app.classes import Track, Album, Artist
+
+
+def lyrics_analyse_artist(artist_name: str) -> dict:
+    return {}
---
+++ backend/app/routes.py
@@ -0,0 +1,27 @@
+"""
+Main backend routing script.
+"""
+
+from flask import jsonify
+from flask import request
+
+from app import APP
+from app import controller
+
+
+@APP.route("/analyse", methods=["GET"])
+def analyse():
+    """
+    """
+    # retrieving artist name from request
+    artist_name = request.args.get("artist")
+
+    # calls method that makes artist lyrics analyse
+    results_of_analyse = controller.lyrics_analyse_artist(artist_name = artist_name)
+
+    return jsonify(
+        {
+            "artist_name": artist_name,
+            "results": results_of_analyse
+        }
+    ), 200
